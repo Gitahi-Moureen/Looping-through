97ac4df4a9a8115eaaac922045a20a04ad662dc8
Gitahi-Moureen <moureengitahiwangari@gmail.com>
2024-04-11 17:41:54 +0300

    Added files

---
+++ function.js
@@ -0,0 +1,31 @@
+//Output all the strings in the following array:
+const animals = ["Dog", "Cat", "Bird", "Lion", "Elephant"];
+for(i=0;i<i.length;i++){
+  
+}
+console.log(animals)
+
+//Write a JavaScript for loop that prints numbers from 10 to 1 (inclusive) to the console in descending order.
+
+function manipulateArr(arr){
+    let newArr = arr.sort()
+        
+    return newArr
+    }
+
+const arr = [1,5,7,8,4,3,8,9,4]
+console.log(manipulateArr(arr))
+
+//Write a JavaScript for loop that prints numbers from 0 to 100 (inclusive) but skips multiples of 5.
+
+// function addition(arr){
+//     let sum = 0 
+//     for (let i = 1; i <=100; i++) {
+//        sum +=arr[i]
+       
+        
+//     }
+// }
+
+// let sum = (1...100)
+// console.log(addition)
